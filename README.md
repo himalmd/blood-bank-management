4d02826f661f1322bfcb194747883da0d96f7ea0
himalmd <dphmalith@gmail.com>
2020-11-01 22:22:12 +0530

    Remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# blood-bank-management
-This is an Online Blood Bank Management System for hospitals and blood banks
--- bank_admin/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-    // Check if the user is logged in, if not then redirect him to login page
-    if(!isset($_SESSION["id-3"]) || !isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ../../reg_login.php");
-    exit;
-	}
-    require_once "../../config.php";
-?>
